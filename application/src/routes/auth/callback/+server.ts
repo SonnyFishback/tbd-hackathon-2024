4e80e26191d89847fe23a3adde32e3d5592c31b7
Bert Bengtson <bertbengtsona@gmail.com>
2024-02-24 17:57:40 -0500

    Add migrations

--- application/src/routes/auth/callback/+server.ts
+++ application/src/routes/auth/callback/+server.ts
@@ -1,20 +1,53 @@
 import { prisma } from '$lib/server/prisma.js'
 import { redirect } from '@sveltejs/kit'
-
+import { error } from '@sveltejs/kit'
 type GoogleMetaData = {
     avatar_url: string
     email: string,
     full_name: string
 }
 export const GET = async ({ url, locals: { supabase, getSession } }) => {
-  const code = url.searchParams.get('code')
+    const code = url.searchParams.get('code')
+    if (!code) {
+        return error(400, 'callback URL was called without a code');
 
-  if (code) {
-    const {data , error}= await supabase.auth.exchangeCodeForSession(code)
-    console.log(data)
+    }
+
+    const response = await supabase.auth.exchangeCodeForSession(code)
+
+    if (response.error) {
+        return error(500, "What happen")
+    }
+
+
+    const user = response.data.user;
+
+    const provider = user.app_metadata.provider;
+
+    if(provider === 'google') {
+        const data  = user.user_metadata as GoogleMetaData;
+
+        await prisma.user.upsert({
+            where: {
+                id: user.id
+            },
+            update: {
+                username: data.email,
+                name: data.full_name,
+                email: data.email,
+                avatar: data.avatar_url
+            },
+            create: {
+                id: user.id,
+                username: data.email,
+                name: data.full_name,
+                email: data.email,
+                avatar: data.avatar_url,
+                password: ''
+            }
+        })
+    }
     return redirect(303, '/')
-  }
- 
-
-
 }
+
+

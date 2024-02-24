3c4ac8b760732c6c927c0e8ed914cf0e218e8e84
jaime1127 <84351799+jaime1127@users.noreply.github.com>
2024-02-24 14:13:08 -0500

    Implementing openAI

---
+++ application/src/routes/interview/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+	import { enhance } from '$app/forms';
+
+	export let data: PageData;
+	export let form: PageData;
+</script>
+
+<div class="div">
+	<form action="?/generate" method="post" use:enhance></form>
+
+	<p>Questions:</p>
+</div>

225fba215054f6786af840dd71a21d4163d267cd
Bert Bengtson <bertbengtsona@gmail.com>
2024-02-24 13:08:05 -0500

    Add Auth

---
+++ application/src/routes/+layout.server.ts
@@ -0,0 +1,7 @@
+import type { LayoutServerLoad } from './$types';
+
+export const load = (async ({ locals }) => {
+    return {
+        session: await locals.getSession(),
+    }
+}) satisfies LayoutServerLoad;

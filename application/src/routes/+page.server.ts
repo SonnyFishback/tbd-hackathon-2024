4a18006a2e2d071e282f50c9e52715ce19e01bae
SystemX <med.fellak@gmail.com>
2024-02-25 12:42:00 -0500

    Remove unused

--- application/src/routes/+page.server.ts
+++ application/src/routes/+page.server.ts
@@ -1,11 +1,7 @@
 import type { PageServerLoad } from './$types';
-import { prisma } from '$lib/server/prisma';
 
 export const load = (async () => {
 
-    const interviewCount = await prisma.interview.count()
     return {
-
-        interviewCount
     };
 }) satisfies PageServerLoad;

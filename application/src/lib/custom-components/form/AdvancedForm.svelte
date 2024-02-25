c873df18a0d3353f698c0ad04c7c4d955929dbcd
SonnyFishback <sonny.fishback7@gmail.com>
2024-02-25 01:43:25 -0500

    update path to submit to correct page.server

--- application/src/lib/custom-components/form/AdvancedForm.svelte
+++ application/src/lib/custom-components/form/AdvancedForm.svelte
@@ -40,7 +40,7 @@
 </script>
 
 <div class="my-4" />
-<form method="POST" action="?/generate" use:enhance>
+<form method="POST" action="/interviews?/generate" use:enhance>
 	<div>
 		<Label>Job Description</Label>
 		<div class="my-4" />

8322d1407029777a4c1196137bf6b360bd954e0b
Bert Bengtson <bertbengtsona@gmail.com>
2024-02-25 13:57:13 -0500

    FUCKING DONE

--- application/src/routes/dashboard/[interview_id]/+page.server.ts
+++ application/src/routes/dashboard/[interview_id]/+page.server.ts
@@ -1,6 +1,6 @@
 import type { PageServerLoad } from './$types';
 import { prisma } from '$lib/server/prisma';
-
+import { redirect } from '@sveltejs/kit';
 export const load = (async ({ params }) => {
     const { interview_id } = params;
     console.log('interview_id', interview_id);
@@ -20,12 +20,23 @@ export const load = (async ({ params }) => {
 }) satisfies PageServerLoad;
 
 export const actions = {
-	default: async ({ request }) => {
+	default: async ({ request, params }) => {
 		const data = await request.formData();
-        console.log(data)
-        for (const [name,value] of data.values()) {
-            console.log(value);
+        const promiseArray = []
+        for (const value of data.values()) {
+                const p = prisma.interviewAnswer.create({
+                    data: {
+                        answerId: value.toString(),
+                        interviewId: params.interview_id
+                    }
+                })
+
+                promiseArray.push(p)
           }
+
+          await Promise.all(promiseArray)
+
+         return redirect(302, `/dashboard/${params.interview_id}/results`)
 		// TODO log the user in
 	}
 };
---
+++ application/src/routes/dashboard/[interview_id]/results/+page.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button';
+    import type { PageData } from './$types';
+    
+    export let data: PageData;
+</script>
+
+
+<section class=" flex flex-col gap-2">
+<h1>
+    Congrats on completeing your Interview!!!
+</h1>
+<ul class=" flex flex-col">
+{#each data.answers as answer}
+    <li class=" text-2xl mb-2">
+        Your answer : {answer.answer.text}
+        {#if answer.answer.isCorrect}
+        <span class=" font-bold"> Is Correct!</span>
+        {/if}
+    </li>
+{/each}
+</ul>
+
+<p>
+    You have earned {data.answers.filter(answer => answer.answer.isCorrect).length} XP
+</p>
+
+<form method="post">
+    <input name="xp" value={data.answers.filter(answer => answer.answer.isCorrect).length} type="hidden" />
+    <Button class="my-3" type="submit">Go to my account</Button>
+</form>
+</section>
